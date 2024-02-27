1dc4836410c52f8de875c8b020748e89c44e7911
Glydewell Burdick <glydewellb@gmail.com>
2024-02-27 11:29:52 -0600

    create basic app

---
+++ app/__init__.py
@@ -0,0 +1,44 @@
+from flask import Flask, jsonify, request
+from mongoengine import connect, Document, StringField
+
+app = Flask(__name__)
+connect('diddy')
+
+class User(Document):
+    name = StringField(required=True, max_length=100)
+    email = StringField(required=True, max_length=100)
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = User.objects()
+    return jsonify(users), 200
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    data = request.get_json()
+    user = User(name=data['name'], email=data['email'])
+    user.save()
+    return jsonify(user), 201
+
+@app.route('/users/<user_id>', methods=['GET'])
+def get_user(user_id):
+    user = User.objects.get(id=user_id)
+    return jsonify(user), 200
+
+@app.route('/users/<user_id>', methods=['PUT'])
+def update_user(user_id):
+    data = request.get_json()
+    user = User.objects.get(id=user_id)
+    user.name = data['name']
+    user.email = data['email']
+    user.save()
+    return jsonify(user), 200
+
+@app.route('/users/<user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    user = User.objects.get(id=user_id)
+    user.delete()
+    return '', 204
+
+if __name__ == '__main__':
+    app.run()
